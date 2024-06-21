a626ee5eee0ed1090235e352be142ee4c7b36751
FedeCati33 <cativellifederico@gmail.com>
2024-06-21 14:06:04 -0300

    route pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { EstadoProveedor } from './context/EstadoGeneral';
-import  Home  from './pages/Home';
+import "./App.css";
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { EstadoProveedor } from "./context/EstadoGeneral";
+
+import AboutUs from "./pages/AboutUs";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <EstadoProveedor>
       <Router>
         <Routes>
-          {/* <Route path="/about" element={<About />} /> */}
-          {/* <Route path="/contact" element={<Contact />} /> */}
           <Route path="/" element={<Home />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </Router>
     </EstadoProveedor>
---
+++ src/pages/AboutUs/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AboutUs = () => {
+  return <p>AboutUs</p>;
+};
+
+export default AboutUs;
