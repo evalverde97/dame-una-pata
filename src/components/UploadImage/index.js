8d3a637b42733e0c0db6d44352c50801f2d0ad99
FedeCati33 <cativellifederico@gmail.com>
2024-07-04 00:18:13 -0300

    uploadIMage

--- src/components/UploadImage/index.js
+++ src/components/UploadImage/index.js
@@ -3,10 +3,13 @@ import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import { doc, setDoc } from "firebase/firestore";
 import { storage, db } from "../../db/firebase";
 
+import ErrorFeddback from "../ErrorFeedback";
+
 import "./index.scss";
 
-const UploadImage = ({id}) => {
+const UploadImage = ({ id }) => {
   const [image, setImage] = useState(null);
+  const [error, setError] = useState(false);
   const [imageUrl, setImageUrl] = useState("");
 
   const handleImageChange = (e) => {
@@ -15,23 +18,32 @@ const UploadImage = ({id}) => {
     }
   };
 
-  const handleUpload = async () => {
-    if (!image) return;
+  const handleClose = (e) => {
+    e.preventdefault();
+    setError(false);
+  };
 
+  const handleUpload = async () => {
     const storageRef = ref(storage, `images/${id}`);
-    try {
-      await uploadBytes(storageRef, image);
-      const url = await getDownloadURL(storageRef);
-      setImageUrl(url);
-      await saveImageUrlToFirestore(url);
-      console.log("Imagen subida y URL guardada en Firestore");
-    } catch (error) {
-      console.error("Error al subir la imagen:", error);
+
+    if (!image) {
+      setError(true);
+      <ErrorFeddback error={error} onClose={handleClose} />;
+    } else {
+      try {
+        await uploadBytes(storageRef, image);
+        const url = await getDownloadURL(storageRef);
+        setImageUrl(url);
+        await saveImageUrlToFirestore(url);
+        console.log("Imagen subida y URL guardada en Firestore");
+      } catch (error) {
+        console.error("Error al subir la imagen:", error);
+      }
     }
   };
 
   const saveImageUrlToFirestore = async (url) => {
-    const docRef = doc(db, 'pets', id);
+    const docRef = doc(db, "pets", id);
     try {
       await setDoc(docRef, { imageUrl: url }, { merge: true });
       console.log("URL de la imagen guardada en Firestore");
