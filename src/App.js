6755fe741d2a704154e05e603b2250e2a674f07f
FedeCati33 <cativellifederico@gmail.com>
2024-07-03 23:07:04 -0300

    add formulario de adopcion

--- src/App.js
+++ src/App.js
@@ -13,7 +13,8 @@ import Contact from "./pages/Contact";
 import Home from "./pages/Home";
 import LogIn from "./pages/Login";
 import MyAccount from "./pages/MyAccount";
-import PetDetail from './pages/PetDetail'
+import PetDetail from "./pages/PetDetail";
+import AdoptionForm from "./pages/AdoptionForm";
 
 const App = () => {
   return (
@@ -27,6 +28,7 @@ const App = () => {
           <Route path="/about" element={<AboutUs />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/account" element={<MyAccount />} />
+          <Route path="/formulario" element={<AdoptionForm />} />
         </Routes>
         <Footer />
       </Router>
